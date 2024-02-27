989c62a804f373a149b1f0143e66af7f487b3090
azuka-p <rad.azka.16@gmail.com>
2024-02-27 19:17:27 +0700

    Implement double-jump, dash, animation

---
+++ README.md
@@ -0,0 +1,8 @@
+https://www.reddit.com/r/godot/comments/lx1eol/need_help_with_double_jump_code/
+https://forum.godotengine.org/t/is-on-floor-not-working-properly/18735
+https://www.youtube.com/watch?v=SN9sOBr3Mp4
+https://www.youtube.com/watch?v=uUTEeriHd_w
+https://www.makeuseof.com/2d-animation-godot-using-animatedsprite/
+https://godotforums.org/d/20957-double-tap-press-to-dash-help
+https://docs.godotengine.org/en/3.6/tutorials/2d/2d_sprite_animation.html
+https://docs.godotengine.org/en/3.5/classes/class_animatedsprite.html
--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,23 +1,61 @@
 extends KinematicBody2D
 
 export (int) var speed = 400
-export (int) var jump_speed = -600
+export (int) var jump_speed = 600
 export (int) var GRAVITY = 1200
 
 const UP = Vector2(0,-1)
 
 var velocity = Vector2()
 
+var jump_count: int = 0
+const max_jumps: int = 2
+
+func jump():
+	if jump_count < max_jumps:
+		jump_count += 1
+		velocity.y = -jump_speed
+		
+var dash_count: int = 0
+var dashing: bool = false
+
+#func dash():
+	#if dash_count < max_dash:
+	#	dash_count += 1
+	#	velocity.y = jump_speed
+
 func get_input():
 	velocity.x = 0
-	if is_on_floor() and Input.is_action_just_pressed('up'):
-		velocity.y = jump_speed
+	if Input.is_action_just_pressed('up'):
+		jump()
+		$AnimatedSprite.play("jump")
+	if Input.is_action_just_pressed('right') and dashing == false and dash_count == 1:
+		print('aaaa')
+		dashing = true
+		velocity.x += 10*speed
+		yield(get_tree().create_timer(.5),"timeout")
+		dashing = false
+	if Input.is_action_just_pressed('right') and dashing == false:
+		dash_count += 1
+		print(dash_count)
+		yield(get_tree().create_timer(.5),"timeout")
+		dash_count = 0
 	if Input.is_action_pressed('right'):
 		velocity.x += speed
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = false
 	if Input.is_action_pressed('left'):
 		velocity.x -= speed
+		$AnimatedSprite.play("walk")
+		$AnimatedSprite.flip_h = true
+	if velocity.x == 0 and velocity.y == 0:
+		$AnimatedSprite.play("idle")
 
 func _physics_process(delta):
-	velocity.y += delta * GRAVITY
 	get_input()
+	velocity.y += delta * GRAVITY
+	
 	velocity = move_and_slide(velocity, UP)
+	
+	if is_on_floor():
+		jump_count = 0
