b5b5a03e4cc73f2a9fc9435b26a02a5b9c76c43d
azuka-p <rad.azka.16@gmail.com>
2024-02-28 18:03:52 +0700

    Implement left-dashing and crouching

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -15,31 +15,42 @@ func jump():
 	if jump_count < max_jumps:
 		jump_count += 1
 		velocity.y = -jump_speed
-		
-var dash_count: int = 0
-var dashing: bool = false
 
-#func dash():
-	#if dash_count < max_dash:
-	#	dash_count += 1
-	#	velocity.y = jump_speed
+var dash_count_r: int = 0
+var dashing_r: bool = false
+
+func dash_right():
+	if Input.is_action_just_pressed('right') and dashing_r == false and dash_count_r == 1:
+		dashing_r = true
+		velocity.x += 4000
+		yield(get_tree().create_timer(.5),"timeout")
+		dashing_r = false
+	if Input.is_action_just_pressed('right') and dashing_r == false:
+		dash_count_r += 1
+		yield(get_tree().create_timer(.2),"timeout")
+		dash_count_r = 0
+
+var dash_count_l: int = 0
+var dashing_l: bool = false
+
+func dash_left():
+	if Input.is_action_just_pressed('left') and dashing_l == false and dash_count_l == 1:
+		dashing_l = true
+		velocity.x -= 4000
+		yield(get_tree().create_timer(.5),"timeout")
+		dashing_l = false
+	if Input.is_action_just_pressed('left') and dashing_l == false:
+		dash_count_l += 1
+		yield(get_tree().create_timer(.2),"timeout")
+		dash_count_l = 0
 
 func get_input():
 	velocity.x = 0
+	dash_right()
+	dash_left()
 	if Input.is_action_just_pressed('up'):
 		jump()
 		$AnimatedSprite.play("jump")
-	if Input.is_action_just_pressed('right') and dashing == false and dash_count == 1:
-		print('aaaa')
-		dashing = true
-		velocity.x += 10*speed
-		yield(get_tree().create_timer(.5),"timeout")
-		dashing = false
-	if Input.is_action_just_pressed('right') and dashing == false:
-		dash_count += 1
-		print(dash_count)
-		yield(get_tree().create_timer(.5),"timeout")
-		dash_count = 0
 	if Input.is_action_pressed('right'):
 		velocity.x += speed
 		$AnimatedSprite.play("walk")
@@ -48,6 +59,10 @@ func get_input():
 		velocity.x -= speed
 		$AnimatedSprite.play("walk")
 		$AnimatedSprite.flip_h = true
+	if Input.is_action_pressed("down"):
+		velocity.y += 5
+		velocity.x /= 2
+		$AnimatedSprite.play("crouch")
 	if velocity.x == 0 and velocity.y == 0:
 		$AnimatedSprite.play("idle")
 
